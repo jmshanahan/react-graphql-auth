3016f7dd83613f295a936774246075145f11dac2
jmshanahan <comeraghsolutions@gmail.com>
2018-04-19 16:07:47 +0100

    Before we add mulkusat code

---
+++ server/schema/mutations.js
@@ -0,0 +1,27 @@
+//import { graphql } from 'react-apollo';
+
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString
+} = graphql;
+const UserType = require('./types/user_types');
+const AuthService = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: {type: GraphQLString},
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, {email, password}, req){
+          return AuthService.signup({ email, password, req});
+      }
+    }
+  }
+})
+
+module.export = mutation;
--- server/server.js
+++ server/server.js
@@ -12,14 +12,15 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+
+const MONGO_URI = 'mongodb://localhost:27017/authgraphql';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI,{useMongoClient: true});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
